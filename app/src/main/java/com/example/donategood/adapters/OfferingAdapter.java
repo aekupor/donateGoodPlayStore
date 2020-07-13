7940e3b9e4ceb9b9d87728c7cef0920e2784a4f5
Ashlee Kupor <aekupor@gmail.com>
2020-07-13 14:50:14 -0700

    [Swipe to Refresh] Add clear and addAll methods to Adapter.

--- app/src/main/java/com/example/donategood/adapters/OfferingAdapter.java
+++ app/src/main/java/com/example/donategood/adapters/OfferingAdapter.java
@@ -118,6 +118,18 @@ public class OfferingAdapter extends RecyclerView.Adapter<OfferingAdapter.ViewHo
              */
         }
 
+        // Clean all elements of the recycler
+        public void clear() {
+            offerings.clear();
+            notifyDataSetChanged();
+        }
+
+        // Add a list of items -- change to type used
+        public void addAll(List<Offering> list) {
+            offerings.addAll(list);
+            notifyDataSetChanged();
+        }
+
         public void onClick(View v) {
             // get item position
             int position = getAdapterPosition();
