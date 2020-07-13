46e9e2155486235abdd8bb52cf37dee4b151df27
Ashlee Kupor <aekupor@gmail.com>
2020-07-13 12:19:16 -0700

    [Offerings] Create RecyclerView.

--- app/src/main/java/com/example/donategood/HomeFragment.java
+++ app/src/main/java/com/example/donategood/HomeFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -14,6 +15,8 @@ import android.view.ViewGroup;
 public class HomeFragment extends Fragment {
 
     public static final String TAG = "HomeFragment";
+
+    private RecyclerView rvOfferings;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -28,6 +31,8 @@ public class HomeFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        rvOfferings = view.findViewById(R.id.rvOfferings);
 
         Log.i(TAG, "onViewCreated in HomeFragment");
     }
