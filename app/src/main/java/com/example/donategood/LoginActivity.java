6a0a0a0c3461885a9adf57b95f0293e30beac0bb
Ashlee Kupor <aekupor@gmail.com>
2020-07-13 10:12:00 -0700

    [Login] Create LoginActivity and set as default screen.

--- app/src/main/java/com/example/donategood/LoginActivity.java
+++ app/src/main/java/com/example/donategood/LoginActivity.java
@@ -3,12 +3,35 @@ package com.example.donategood;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    public static final String TAG = "LoginActivity";
+
+    private EditText etUsername;
+    private EditText etPassword;
+    private Button btnLogin;
+    private Button btnRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        etUsername = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnRegister = findViewById(R.id.btnRegister);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i(TAG, "onClick login button");
+            }
+        });
     }
 }
