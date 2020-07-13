429a87da579c95ecc6e4cbbadfb3d7c790052d06
Ashlee Kupor <aekupor@gmail.com>
2020-07-13 11:29:15 -0700

    [Nav bar] Create onClick listeners for bottom nav bar.

--- app/src/main/java/com/example/donategood/MainActivity.java
+++ app/src/main/java/com/example/donategood/MainActivity.java
@@ -1,14 +1,49 @@
 package com.example.donategood;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "MainActivity";
+
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.action_profile:
+                        Log.i(TAG, "profile button clicked");
+                        break;
+                    case R.id.action_compose:
+                        Log.i(TAG, "compose button clicked");
+                        break;
+                    case R.id.action_search:
+                        Log.i(TAG, "search button clicked");
+                        break;
+                    default:
+                        //home button clicked
+                        Log.i(TAG, "home button clicked");
+                        break;
+                }
+                return true;
+            }
+        });
+        //set default selection
+        bottomNavigationView.setSelectedItemId(R.id.action_home);
     }
 }
