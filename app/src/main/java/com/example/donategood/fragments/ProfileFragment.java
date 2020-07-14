96c02f7a858f8445eb71b5ceaef148afe20155bb
Ashlee Kupor <aekupor@gmail.com>
2020-07-14 14:54:03 -0700

    [Profile] Allow user to take or upload profile photo.

--- app/src/main/java/com/example/donategood/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/donategood/fragments/ProfileFragment.java
@@ -1,30 +1,53 @@
 package com.example.donategood.fragments;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.donategood.Camera;
 import com.example.donategood.LoadPost;
 import com.example.donategood.LoginActivity;
 import com.example.donategood.R;
+import com.example.donategood.models.Charity;
+import com.parse.FindCallback;
+import com.parse.ParseException;
 import com.parse.ParseUser;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import static android.app.Activity.RESULT_OK;
 
 public class ProfileFragment extends Fragment {
 
     public static final String TAG = "ProfileFragment";
+    public static final Integer CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 10;
+    public static final Integer UPLOAD_PHOTO_CODE = 20;
 
     private LoadPost loadPost;
+    private Camera camera;
+    private File photoFile;
+    public String photoFileName = "photo.jpg";
 
     private Button btnLogout;
     private Button btnTakePhoto;
@@ -53,8 +76,23 @@ public class ProfileFragment extends Fragment {
         btnUploadPhoto = view.findViewById(R.id.btnProfileUploadPhoto);
 
         loadPost = new LoadPost();
+        camera = new Camera();
 
         loadPost.setUser(ParseUser.getCurrentUser(), getContext(), tvName, ivProfileImage);
+
+        btnTakePhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchCamera();
+            }
+        });
+
+        btnUploadPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pickPhoto();
+            }
+        });
 
         btnLogout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -65,5 +103,55 @@ public class ProfileFragment extends Fragment {
                 startActivity(i);
             }
         });
+    }
+
+    protected void launchCamera() {
+        // create Intent to take a picture and return control to the calling application
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Create a File reference for future access
+        photoFile = camera.getPhotoFileUri(photoFileName, getContext());
+
+        // wrap File object into a content provider; required for API >= 24
+        Uri fileProvider = FileProvider.getUriForFile(getContext(), "com.codepath.fileprovider.donateGood", photoFile);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
+
+        if (intent.resolveActivity(getContext().getPackageManager()) != null) {
+            // Start the image capture intent to take photo
+            startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+        }
+    }
+
+    public void pickPhoto() {
+        // Create intent for picking a photo from the gallery
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+        if (intent.resolveActivity(getContext().getPackageManager()) != null) {
+            // Bring up gallery to select a photo
+            startActivityForResult(intent, UPLOAD_PHOTO_CODE);
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                // by this point we have the camera photo on disk
+                Bitmap takenImage = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
+                // Load the taken image into a preview
+                ivProfileImage.setImageBitmap(takenImage);
+            } else { // Result was a failure
+                Toast.makeText(getContext(), "Picture wasn't taken!", Toast.LENGTH_SHORT).show();
+            }
+        } else if ((data != null) && requestCode == UPLOAD_PHOTO_CODE) {
+            Uri photoUri = data.getData();
+
+            // Load the image located at photoUri into selectedImage
+            Bitmap selectedImage = camera.loadFromUri(photoUri, getContext());
+
+            // Load the selected image into a preview
+            ivProfileImage.setImageBitmap(selectedImage);
+            photoFile = camera.createFile(getContext(), selectedImage);
+        }
     }
 }
