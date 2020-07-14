f4b0d2394718942c972b61663bb0aa8c6306aec0
Ashlee Kupor <aekupor@gmail.com>
2020-07-14 15:31:59 -0700

    [Other Profile] Create other user profile page.

---
+++ app/src/main/java/com/example/donategood/fragments/OtherUserProfileFragment.java
@@ -0,0 +1,71 @@
+package com.example.donategood.fragments;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.donategood.LoadPost;
+import com.example.donategood.R;
+import com.parse.ParseUser;
+
+
+public class OtherUserProfileFragment extends Fragment {
+
+    public static final String TAG = "OtherUserProfileFragment";
+
+    private LoadPost loadPost;
+
+    private TextView tvName;
+    private ImageView ivProfileImage;
+
+    private String userName;
+    private ParseUser user;
+
+    public OtherUserProfileFragment() {
+        // Required empty public constructor
+    }
+
+    public static OtherUserProfileFragment newInstance(String userName) {
+        OtherUserProfileFragment fragment = new OtherUserProfileFragment();
+        Bundle args = new Bundle();
+        args.putString("userName", userName);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @SuppressLint("LongLogTag")
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        userName = getArguments().getString("userName", "");
+        Log.i(TAG, "userName: " + userName);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_other_user_profile, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        tvName = view.findViewById(R.id.tvOtherProfileProfileName);
+        ivProfileImage = view.findViewById(R.id.ivOtherProfileProfileImage);
+
+        loadPost = new LoadPost();
+
+        //loadPost.setUser(ParseUser.getCurrentUser(), getContext(), tvName, ivProfileImage);
+    }
+}
