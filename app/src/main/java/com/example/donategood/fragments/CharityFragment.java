c60001ca51a2ad08f884cecdb7c81344ddcf24e3
Ashlee Kupor <aekupor@gmail.com>
2020-07-14 14:19:07 -0700

    [Charity] Create charity fragment.

---
+++ app/src/main/java/com/example/donategood/fragments/CharityFragment.java
@@ -0,0 +1,36 @@
+package com.example.donategood.fragments;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.donategood.R;
+
+public class CharityFragment extends Fragment {
+
+    public static final String TAG = "CharityFragment";
+
+    public CharityFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_charity, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Log.i(TAG, "onViewCreated in CharityFragment");
+    }
+}
