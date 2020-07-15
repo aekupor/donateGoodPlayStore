0ffa138bcb34dc856fc1b7a9407e4cd2de0c912f
Ashlee Kupor <aekupor@gmail.com>
2020-07-15 15:05:33 -0700

    [UI] Create SmallViewHolder that extends ViewHolder.

--- app/src/main/java/com/example/donategood/adapters/SmallOfferingAdapter.java
+++ app/src/main/java/com/example/donategood/adapters/SmallOfferingAdapter.java
@@ -4,10 +4,13 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 
 import com.example.donategood.R;
+import com.example.donategood.helperClasses.LoadPost;
 import com.example.donategood.models.Offering;
 
 import java.util.List;
@@ -20,8 +23,35 @@ public class SmallOfferingAdapter extends OfferingAdapter {
 
     @NonNull
     @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(this.context).inflate(R.layout.item_offering, parent, false);
-        return new ViewHolder(view);
+    public SmallViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(this.context).inflate(R.layout.item_small_offering, parent, false);
+        return new SmallViewHolder(view);
+    }
+
+    class SmallViewHolder extends ViewHolder {
+
+        private TextView tvTitle;
+        private TextView tvPrice;
+        private TextView tvUser;
+        private ImageView ivOfferingPhoto;
+
+        private LoadPost loadPost;
+
+        public SmallViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvTitle = itemView.findViewById(R.id.tvTitle);
+            tvPrice = itemView.findViewById(R.id.tvPrice);
+            tvUser = itemView.findViewById(R.id.tvUser);
+            ivOfferingPhoto = itemView.findViewById(R.id.ivOfferingPhoto);
+
+            loadPost = new LoadPost();
+
+            itemView.setOnClickListener(this);
+        }
+
+        public void bind(Offering offering) {
+            loadPost.setTitlePriceUser(offering, tvTitle, tvPrice, tvUser);
+            loadPost.setPostImage(offering.getImage(), context, ivOfferingPhoto);
+        }
     }
 }
