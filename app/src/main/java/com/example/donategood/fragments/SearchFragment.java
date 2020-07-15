815ecbeaae745c31a5135047d4d043c5e42f8222
Ashlee Kupor <aekupor@gmail.com>
2020-07-15 11:01:06 -0700

    [Search] Create search fragment layout.

--- app/src/main/java/com/example/donategood/fragments/SearchFragment.java
+++ app/src/main/java/com/example/donategood/fragments/SearchFragment.java
@@ -5,17 +5,24 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.donategood.R;
 
 public class SearchFragment extends Fragment {
 
     public static final String TAG = "SearchFragment";
+
+    private EditText etSearchText;
+    private Button btnSearch;
+    private RecyclerView rvOfferings;
 
     public SearchFragment() {
         // Required empty public constructor
@@ -31,6 +38,8 @@ public class SearchFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        Log.i(TAG, "onViewCreated in SearchFragment");
+        etSearchText = view.findViewById(R.id.etSearchBar);
+        btnSearch = view.findViewById(R.id.btnSearch);
+        rvOfferings = view.findViewById(R.id.rvSearchOfferings);
     }
 }
