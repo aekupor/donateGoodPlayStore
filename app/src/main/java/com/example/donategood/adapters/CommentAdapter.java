9327e7a8d94d622330132c7af342faf0027e8ec5
Ashlee Kupor <aekupor@gmail.com>
2020-07-17 09:24:03 -0700

    [Comment] Create bind method on comment adapter.

--- app/src/main/java/com/example/donategood/adapters/CommentAdapter.java
+++ app/src/main/java/com/example/donategood/adapters/CommentAdapter.java
@@ -5,11 +5,15 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.donategood.R;
+import com.example.donategood.helperClasses.LoadPost;
 import com.example.donategood.models.Comment;
 
 import java.util.List;
@@ -57,15 +61,30 @@ public class CommentAdapter extends RecyclerView.Adapter<CommentAdapter.ViewHold
 
     class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
+        TextView tvUsername;
+        TextView tvDescription;
+        ImageView ivProfileImage;
+        RatingBar rbRating;
+
+        LoadPost loadPost;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            tvUsername = itemView.findViewById(R.id.tvUsernameComment);
+            tvDescription = itemView.findViewById(R.id.tvTextComment);
+            ivProfileImage = itemView.findViewById(R.id.ivProfileImageComment);
+            rbRating = itemView.findViewById(R.id.tvRatingComment);
+
+            loadPost = new LoadPost();
 
             itemView.setOnClickListener(this);
         }
 
         public void bind(Comment comment) {
-
+            loadPost.setUser(comment.getByUser(), context, tvUsername, ivProfileImage);
+            tvDescription.setText(comment.getText());
+            rbRating.setNumStars(comment.getRating());
         }
 
         public void onClick(View v) {
