db8059d875894ea91fcf7a5b03791137b18ebba1
Ashlee Kupor <aekupor@gmail.com>
2020-07-17 09:43:28 -0700

    [Comment] Create ComposeCommentFragment.

---
+++ app/src/main/java/com/example/donategood/fragments/ComposeCommentFragment.java
@@ -0,0 +1,47 @@
+package com.example.donategood.fragments;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+import com.example.donategood.R;
+
+public class ComposeCommentFragment extends DialogFragment {
+
+        private EditText tvCommentText;
+        private EditText tvCommentRating;
+
+        public ComposeCommentFragment() {
+                // Empty constructor is required for DialogFragment
+        }
+
+        public static ComposeCommentFragment newInstance() {
+                ComposeCommentFragment frag = new ComposeCommentFragment();
+                Bundle args = new Bundle();
+                frag.setArguments(args);
+                return frag;
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+                return inflater.inflate(R.layout.fragment_compose_comment, container, false);
+        }
+
+        @Override
+        public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+                super.onViewCreated(view, savedInstanceState);
+
+                tvCommentText = (EditText) view.findViewById(R.id.etTestComment);
+                tvCommentRating = (EditText) view.findViewById(R.id.etRatingComment);
+
+                // Show soft keyboard automatically and request focus to field
+                tvCommentText.requestFocus();
+
+        }
+}
