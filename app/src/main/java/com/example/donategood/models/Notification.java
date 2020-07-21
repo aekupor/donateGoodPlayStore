084ee1c6be5df05b7728eb5cc335d255ba6ec1e6
Ashlee Kupor <aekupor@gmail.com>
2020-07-21 09:51:45 -0700

    [Notifications] Create getters and setters for notification model.

--- app/src/main/java/com/example/donategood/models/Notification.java
+++ app/src/main/java/com/example/donategood/models/Notification.java
@@ -2,6 +2,7 @@ package com.example.donategood.models;
 
 import com.parse.ParseClassName;
 import com.parse.ParseObject;
+import com.parse.ParseUser;
 
 @ParseClassName("Notification")
 public class Notification extends ParseObject {
@@ -10,4 +11,27 @@ public class Notification extends ParseObject {
     public static final String KEY_OFFERING = "forOffering";
     public static final String KEY_APPROVED = "approved";
 
+    public ParseUser getKeyUser() {
+        return getParseUser(KEY_USER);
+    }
+
+    public void setKeyUser(ParseUser user) {
+        put(KEY_USER, user);
+    }
+
+    public Offering getKeyOffering() {
+        return (Offering) get(KEY_OFFERING);
+    }
+
+    public void setKeyOffering(Offering offering) {
+        put(KEY_OFFERING, offering);
+    }
+
+    public Boolean getKeyApproved() {
+        return getBoolean(KEY_APPROVED);
+    }
+
+    public void setKeyApproved(Boolean approved) {
+        put(KEY_APPROVED, approved);
+    }
 }
