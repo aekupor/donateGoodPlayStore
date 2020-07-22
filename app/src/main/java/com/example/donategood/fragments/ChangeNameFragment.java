37a76428513f83e612210c4bfef76b77481fa427
Ashlee Kupor <aekupor@gmail.com>
2020-07-22 14:09:27 -0700

    [Profile UI] Create change name fragment.

---
+++ app/src/main/java/com/example/donategood/fragments/ChangeNameFragment.java
@@ -0,0 +1,47 @@
+package com.example.donategood.fragments;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.donategood.R;
+
+public class ChangeNameFragment extends DialogFragment {
+
+    private EditText tvName;
+    private Button btnSubmit;
+
+    public ChangeNameFragment() {
+        // Empty constructor is required for DialogFragment
+    }
+
+    // Defines the listener interface
+    public interface ChangeNameDialogListener {
+        void onFinishEditDialog(String inputText);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_change_name, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+
+    // sends the data back to the parent fragment
+    public void sendBackResult() {
+        ChangeNameDialogListener listener = (ChangeNameDialogListener) getTargetFragment();
+        listener.onFinishEditDialog("temp");
+        dismiss();
+    }
+}
