b10bb4fd29771123b6701cf9fd53866589058171
Ashlee Kupor <aekupor@gmail.com>
2020-07-23 09:18:16 -0700

    [Register UI] Create logic for register page.

--- app/src/main/java/com/example/donategood/RegisterActivity.java
+++ app/src/main/java/com/example/donategood/RegisterActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -17,14 +18,43 @@ public class RegisterActivity extends AppCompatActivity {
 
     public static final String TAG = "RegisterActivity";
 
+    private EditText etUsername;
+    private EditText etEmail;
+    private EditText etPassword;
+    private EditText etPasswordConfirm;
+    private Button btnSubmit;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        etUsername = findViewById(R.id.etUsernameRegister);
+        etEmail = findViewById(R.id.etEmailRegister);
+        etPassword = findViewById(R.id.etPasswordRegister);
+        etPasswordConfirm = findViewById(R.id.etPasswordConfirmRegister);
+        btnSubmit = findViewById(R.id.btnRegisterRegister);
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = etUsername.getText().toString();
+                String password = etPassword.getText().toString();
+                String confirmPassword = etPasswordConfirm.getText().toString();
+                String email = etEmail.getText().toString();
+
+                if (!confirmPassword.equals(password)) {
+                    Toast.makeText(RegisterActivity.this, "Passwords do not match", Toast.LENGTH_SHORT).show();
+                } else if (username.isEmpty() || password.isEmpty() || email.isEmpty()) {
+                    Toast.makeText(RegisterActivity.this, "Field cannot be empty!", Toast.LENGTH_SHORT).show();
+                } else {
+                    registerUser(username, password, email);
+                }
+            }
+        });
     }
 
-    private void registerUser(String username, String password) {
+    private void registerUser(String username, String password, String email) {
         Log.i(TAG, "attempting to register user " + username);
 
         // Create the ParseUser
@@ -32,6 +62,7 @@ public class RegisterActivity extends AppCompatActivity {
         // Set core properties
         user.setUsername(username);
         user.setPassword(password);
+        user.setEmail(email);
         // Invoke signUpInBackground
         user.signUpInBackground(new SignUpCallback() {
             public void done(ParseException e) {
