1fd705d3c63e1282f04b4e5618fd75e146e3da48
Ashlee Kupor <aekupor@gmail.com>
2020-07-23 10:57:02 -0700

    [Profile Inheritance] Move checkFBLogin to FBQuery class.

--- app/src/main/java/com/example/donategood/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/donategood/fragments/ProfileFragment.java
@@ -21,13 +21,7 @@ import com.example.donategood.R;
 import com.example.donategood.helperClasses.Camera;
 import com.example.donategood.helperClasses.FBQuery;
 import com.example.donategood.helperClasses.ParentProfile;
-import com.facebook.AccessToken;
-import com.facebook.GraphRequest;
-import com.facebook.GraphResponse;
 import com.parse.ParseUser;
-
-import org.json.JSONException;
-import org.json.JSONObject;
 
 public class ProfileFragment extends Fragment implements ChangeNameFragment.ChangeNameDialogListener {
 
@@ -99,55 +93,12 @@ public class ProfileFragment extends Fragment implements ChangeNameFragment.Chan
 
         camera = new Camera();
 
-        checkFBLogin();
+        FBQuery fbQuery = new FBQuery();
+        fbQuery.checkFBLogin(parentProfile, getContext());
     }
 
     public static Camera getCamera() {
         return camera;
-    }
-
-    public void checkFBLogin() {
-        final AccessToken accessToken = AccessToken.getCurrentAccessToken();
-        boolean isLoggedIn = accessToken != null && !accessToken.isExpired();
-        if (isLoggedIn) {
-            //user is logged in with facebook
-            Log.i(TAG, "user is logged in with FB");
-            final Long[] userId = new Long[1];
-            final FBQuery fbQuery = new FBQuery();
-
-            //get user name from FB
-            fbQuery.getName(accessToken, new GraphRequest.GraphJSONObjectCallback() {
-                @Override
-                public void onCompleted(JSONObject object, GraphResponse response) {
-                    try {
-                        final String name = object.getString("name");
-                        Log.i(TAG, "got graph response: " + name);
-                        userId[0] = object.getLong("id");
-
-                        //get user profile picture from FB
-                        fbQuery.getProfileImage(accessToken, userId[0], new GraphRequest.Callback() {
-                            @Override
-                            public void onCompleted(GraphResponse response) {
-                                try {
-                                    JSONObject data = response.getJSONObject().getJSONObject("data");
-                                    String url = data.getString("url");
-                                    Log.i(TAG, "got image url: " + url);
-
-                                    parentProfile.loadPost.setUserFromFB(name, url, getContext(), parentProfile.tvName, parentProfile.ivProfileImage);
-                                } catch (JSONException e) {
-                                    e.printStackTrace();
-                                }
-                            }
-                        });
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
-                }
-            });
-        } else {
-            //user is not logged in with FB
-            parentProfile.loadPost.setUser(ParseUser.getCurrentUser(), getContext(), parentProfile.tvName, parentProfile.ivProfileImage);
-        }
     }
 
     private void showEditDialog() {
--- app/src/main/java/com/example/donategood/helperClasses/FBQuery.java
+++ app/src/main/java/com/example/donategood/helperClasses/FBQuery.java
@@ -1,11 +1,20 @@
 package com.example.donategood.helperClasses;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.facebook.AccessToken;
 import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.parse.ParseUser;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class FBQuery {
+
+    public static final String TAG = "FBQuery";
 
     public void getName(AccessToken accessToken, GraphRequest.GraphJSONObjectCallback graphJSONObjectCallback) {
         GraphRequest request = GraphRequest.newMeRequest(
@@ -26,5 +35,49 @@ public class FBQuery {
                 callback);
 
         photoRequest.executeAsync();
+    }
+
+    public void checkFBLogin(final ParentProfile parentProfile, final Context context) {
+        final AccessToken accessToken = AccessToken.getCurrentAccessToken();
+        boolean isLoggedIn = accessToken != null && !accessToken.isExpired();
+        if (isLoggedIn) {
+            //user is logged in with facebook
+            Log.i(TAG, "user is logged in with FB");
+            final Long[] userId = new Long[1];
+            final FBQuery fbQuery = new FBQuery();
+
+            //get user name from FB
+            fbQuery.getName(accessToken, new GraphRequest.GraphJSONObjectCallback() {
+                @Override
+                public void onCompleted(JSONObject object, GraphResponse response) {
+                    try {
+                        final String name = object.getString("name");
+                        Log.i(TAG, "got graph response: " + name);
+                        userId[0] = object.getLong("id");
+
+                        //get user profile picture from FB
+                        fbQuery.getProfileImage(accessToken, userId[0], new GraphRequest.Callback() {
+                            @Override
+                            public void onCompleted(GraphResponse response) {
+                                try {
+                                    JSONObject data = response.getJSONObject().getJSONObject("data");
+                                    String url = data.getString("url");
+                                    Log.i(TAG, "got image url: " + url);
+
+                                    parentProfile.loadPost.setUserFromFB(name, url, context, parentProfile.tvName, parentProfile.ivProfileImage);
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        });
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        } else {
+            //user is not logged in with FB
+            parentProfile.loadPost.setUser(ParseUser.getCurrentUser(), context, parentProfile.tvName, parentProfile.ivProfileImage);
+        }
     }
 }
