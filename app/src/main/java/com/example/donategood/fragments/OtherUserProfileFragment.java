47d78f343841c370c38212889f8b46c6ca5d67aa
Ashlee Kupor <aekupor@gmail.com>
2020-07-23 09:38:20 -0700

    [Profile Inheritance] Refactor OtherUserProfileFragment to call from ParentProfile.

--- app/src/main/java/com/example/donategood/fragments/OtherUserProfileFragment.java
+++ app/src/main/java/com/example/donategood/fragments/OtherUserProfileFragment.java
@@ -4,36 +4,25 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.ProgressBar;
-import android.widget.RatingBar;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import com.example.donategood.adapters.SmallOfferingAdapter;
-import com.example.donategood.helperClasses.LoadPost;
-import com.example.donategood.helperClasses.Query;
 import com.example.donategood.R;
-import com.example.donategood.models.Offering;
 import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseUser;
 
-import java.util.ArrayList;
 import java.util.List;
 
 
@@ -44,21 +33,8 @@ public class OtherUserProfileFragment extends Fragment {
     public static final String KEY_SELLING = "selling";
     public static final String KEY_SOLD = "sold";
 
-    private LoadPost loadPost;
-    private Query query;
-
-    private TextView tvName;
-    private ImageView ivProfileImage;
-    private TextView tvMoneyRaised;
-    private RecyclerView rvOfferings;
-    private SmallOfferingAdapter adapter;
-    private List<Offering> selectedOfferings;
-    private TextView tvBoughtTitle;
-    private TextView tvSoldTitle;
-    private TextView tvSellingTitle;
-    private ProgressBar pb;
+    private ParentProfileFragment parentProfile;
     private Button btnChat;
-    private RatingBar ratingBar;
 
     private String userName;
     private ParseUser user;
@@ -101,53 +77,33 @@ public class OtherUserProfileFragment extends Fragment {
             return;
         }
 
-        ParentProfileFragment parentProfile = new ParentProfileFragment();
+        parentProfile = new ParentProfileFragment();
         parentProfile.initializeVariables(view, getContext());
 
-
-        tvName = view.findViewById(R.id.tvOtherProfileProfileName);
-        ivProfileImage = view.findViewById(R.id.ivOtherProfileProfileImage);
-        tvMoneyRaised = view.findViewById(R.id.tvOtherUserMoneyRaised);
-        rvOfferings = view.findViewById(R.id.rvOtherUserSelling);
-        tvBoughtTitle = view.findViewById(R.id.tvOtherBoughtTitle);
-        tvSellingTitle = view.findViewById(R.id.tvOtherSellingTitle);
-        tvSoldTitle = view.findViewById(R.id.tvOtherSoldTitle);
-        pb = (ProgressBar) view.findViewById(R.id.pbOtherProfileLoading);
         btnChat = view.findViewById(R.id.btnChat);
-        ratingBar = (RatingBar) view.findViewById(R.id.rbOtherUserProfile);
 
-        selectedOfferings = new ArrayList<>();
-        adapter = new SmallOfferingAdapter(getContext(), selectedOfferings);
-
-        rvOfferings.setAdapter(adapter);
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
-        rvOfferings.setLayoutManager(linearLayoutManager);
-
-        loadPost = new LoadPost();
-        query = new Query();
-
-        tvBoughtTitle.setOnClickListener(new View.OnClickListener() {
+        parentProfile.tvBoughtTitle.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 queryPosts(KEY_BOUGHT);
             }
         });
 
-        tvSellingTitle.setOnClickListener(new View.OnClickListener() {
+        parentProfile.tvSellingTitle.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 queryPosts(KEY_SELLING);
             }
         });
 
-        tvSoldTitle.setOnClickListener(new View.OnClickListener() {
+        parentProfile.tvSoldTitle.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 queryPosts(KEY_SOLD);
             }
         });
 
-        query.queryUserByName(userName, new FindCallback<ParseUser>() {
+        parentProfile.query.queryUserByName(userName, new FindCallback<ParseUser>() {
             @Override
             public void done(List<ParseUser> objects, ParseException e) {
                 if (e != null) {
@@ -155,12 +111,12 @@ public class OtherUserProfileFragment extends Fragment {
                     return;
                 }
                 user = objects.get(0);
-                loadPost.setUser(user, getContext(), tvName, ivProfileImage);
+                parentProfile.loadPost.setUser(user, getContext(), parentProfile.tvName, parentProfile.ivProfileImage);
 
                 queryPosts(KEY_BOUGHT);
 
-                query.queryMoneyRaised(user, tvMoneyRaised);
-                query.queryUserRating(user, ratingBar);
+                parentProfile.query.queryMoneyRaised(user, parentProfile.tvMoneyRaised);
+                parentProfile.query.queryUserRating(user, parentProfile.ratingBar);
             }
         });
 
@@ -179,8 +135,8 @@ public class OtherUserProfileFragment extends Fragment {
     }
 
     protected void queryPosts(String queryType) {
-        pb.setVisibility(ProgressBar.VISIBLE);
-        query.setBold(queryType, tvSoldTitle, tvSellingTitle, tvBoughtTitle);
-        query.queryPosts(user, queryType, adapter, selectedOfferings, pb);
+        parentProfile.pb.setVisibility(ProgressBar.VISIBLE);
+        parentProfile.query.setBold(queryType, parentProfile.tvSoldTitle, parentProfile.tvSellingTitle, parentProfile.tvBoughtTitle);
+        parentProfile.query.queryPosts(user, queryType, parentProfile.adapter, parentProfile.selectedOfferings, parentProfile.pb);
     }
 }
