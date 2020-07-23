0bacbb248b6a463f12bbb17d17c0590f714ac730
Ashlee Kupor <aekupor@gmail.com>
2020-07-23 09:29:57 -0700

    [Profile Inheritance] Create initializeVariables method.

--- app/src/main/java/com/example/donategood/fragments/ParentProfileFragment.java
+++ app/src/main/java/com/example/donategood/fragments/ParentProfileFragment.java
@@ -1,46 +1,66 @@
 package com.example.donategood.fragments;
 
-import android.os.Bundle;
-import android.util.Log;
+import android.content.Context;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.donategood.R;
+import com.example.donategood.adapters.SmallOfferingAdapter;
+import com.example.donategood.helperClasses.LoadPost;
+import com.example.donategood.helperClasses.Query;
+import com.example.donategood.models.Offering;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
-public class ParentProfileFragment extends Fragment {
+public class ParentProfileFragment {
 
     public static final String TAG = "ParentProfileFragment";
 
-    private String userName;
+    private LoadPost loadPost;
+    private Query query;
 
-    public ParentProfileFragment() {
-        // Required empty public constructor
-    }
+    private TextView tvName;
+    private ImageView ivProfileImage;
+    private TextView tvMoneyRaised;
+    private RecyclerView rvOfferings;
+    private SmallOfferingAdapter adapter;
+    private List<Offering> selectedOfferings;
+    private TextView tvBoughtTitle;
+    private TextView tvSoldTitle;
+    private TextView tvSellingTitle;
+    private ProgressBar pb;
+    private Button btnChat;
+    private RatingBar ratingBar;
 
-    public static ParentProfileFragment newInstance(String userName) {
-        ParentProfileFragment fragment = new ParentProfileFragment();
-        Bundle args = new Bundle();
-        args.putString("userName", userName);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    protected void initializeVariables(View view, Context context) {
+        tvName = view.findViewById(R.id.tvOtherProfileProfileName);
+        ivProfileImage = view.findViewById(R.id.ivOtherProfileProfileImage);
+        tvMoneyRaised = view.findViewById(R.id.tvOtherUserMoneyRaised);
+        rvOfferings = view.findViewById(R.id.rvOtherUserSelling);
+        tvBoughtTitle = view.findViewById(R.id.tvOtherBoughtTitle);
+        tvSellingTitle = view.findViewById(R.id.tvOtherSellingTitle);
+        tvSoldTitle = view.findViewById(R.id.tvOtherSoldTitle);
+        pb = (ProgressBar) view.findViewById(R.id.pbOtherProfileLoading);
+        btnChat = view.findViewById(R.id.btnChat);
+        ratingBar = (RatingBar) view.findViewById(R.id.rbOtherUserProfile);
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        userName = getArguments().getString("userName", "");
-        Log.i(TAG, "userName: " + userName);
-    }
+        selectedOfferings = new ArrayList<>();
+        adapter = new SmallOfferingAdapter(context, selectedOfferings);
 
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
+        rvOfferings.setAdapter(adapter);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(context);
+        rvOfferings.setLayoutManager(linearLayoutManager);
 
-    }
-
-    protected void queryPosts(String queryType) {
-
+        loadPost = new LoadPost();
+        query = new Query();
     }
 }
