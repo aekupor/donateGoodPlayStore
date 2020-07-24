fdf2d2f6b73eead280d99dfe6ea9c38e4a8572df
Ashlee Kupor <aekupor@gmail.com>
2020-07-24 11:29:10 -0700

    [Search by User/Charity] Create user adapter.

---
+++ app/src/main/java/com/example/donategood/adapters/UserAdapter.java
@@ -0,0 +1,95 @@
+package com.example.donategood.adapters;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.donategood.R;
+import com.example.donategood.helperClasses.LoadPost;
+import com.parse.ParseUser;
+
+import java.util.List;
+
+public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
+
+    public static final String TAG = "UserAdapter";
+
+    private Context context;
+    private List<ParseUser> users;
+
+    public UserAdapter(Context context, List<ParseUser> users) {
+        this.context = context;
+        this.users = users;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.item_user, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        ParseUser user = users.get(position);
+        holder.bind(user);
+    }
+
+    @Override
+    public int getItemCount() {
+        return users.size();
+    }
+
+    // Clean all elements of the recycler
+    public void clear() {
+        users.clear();
+        notifyDataSetChanged();
+    }
+
+    public void addAll(List<ParseUser> list) {
+        users.addAll(list);
+        notifyDataSetChanged();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        TextView tvUsername;
+        ImageView ivProfileImage;
+
+        LoadPost loadPost;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            tvUsername = itemView.findViewById(R.id.tvUserUsername);
+            ivProfileImage = itemView.findViewById(R.id.ivUserProfileImage);
+
+            loadPost = new LoadPost();
+
+            itemView.setOnClickListener(this);
+        }
+
+        public void bind(ParseUser user) {
+            loadPost.setUser(user, context, tvUsername, ivProfileImage);
+        }
+
+        public void onClick(View v) {
+            // gets item position
+            int position = getAdapterPosition();
+            // make sure the position is valid, i.e. actually exists in the view
+            if (position != RecyclerView.NO_POSITION) {
+                // get the post at the position
+                ParseUser user = users.get(position);
+
+                Log.i(TAG, "user clicked: " + user.getUsername());
+            }
+        }
+    }
+}
