88c78a2742234fa90750cb0a6bb6b35b47a833ca
Ashlee Kupor <aekupor@gmail.com>
2020-07-27 12:42:40 -0700

    [Chat] Create basic design for chat activity.

---
+++ app/src/main/java/com/example/donategood/ChatActivity.java
@@ -0,0 +1,16 @@
+package com.example.donategood;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ChatActivity extends AppCompatActivity {
+    static final String TAG = ChatActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+
+    }
+}
