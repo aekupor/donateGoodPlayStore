d5816669afbe92473333c220ae71ec20007d20d2
Ashlee Kupor <aekupor@gmail.com>
2020-07-28 08:31:30 -0700

    [Chat List] Create basic chat list fragment.

--- app/src/main/java/com/example/donategood/fragments/ChatListFragment.java
+++ app/src/main/java/com/example/donategood/fragments/ChatListFragment.java
@@ -1,6 +1,35 @@
 package com.example.donategood.fragments;
 
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.donategood.R;
+
 public class ChatListFragment extends Fragment {
+
+    public static final String TAG = "ChatListFragment";
+
+    public ChatListFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_chat_list, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Log.i(TAG, "onViewCreated chat list fragment");
+    }
 }
