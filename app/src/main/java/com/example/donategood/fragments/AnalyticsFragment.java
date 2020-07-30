7a0ec7288c0f733d4bf83e4a5ab9be59be090edb
Ashlee Kupor <aekupor@gmail.com>
2020-07-30 15:45:20 -0700

    Create example charity money raised pie chart.

--- app/src/main/java/com/example/donategood/fragments/AnalyticsFragment.java
+++ app/src/main/java/com/example/donategood/fragments/AnalyticsFragment.java
@@ -70,41 +70,22 @@ public class AnalyticsFragment extends DialogFragment {
         }
 
         tvAnalytics.setText(tvAnalytics.getText() + "Amazing job!");
-
-
+        
         pieChart = view.findViewById(R.id.piechartAnalytics);
         createPieChart();
     }
 
     public void createPieChart() {
-        List<PieEntry> NoOfEmp = new ArrayList();
+        List<PieEntry> entries = new ArrayList();
+        entries.add(new PieEntry(750f, "Charity1"));
+        entries.add(new PieEntry(300f, "Charity2"));
+        entries.add(new PieEntry(150f, "Charity3"));
+        entries.add(new PieEntry(20f, "Charity4"));
 
-        NoOfEmp.add(new PieEntry(945f, 0));
-        NoOfEmp.add(new PieEntry(1040f, 1));
-        NoOfEmp.add(new PieEntry(1133f, 2));
-        NoOfEmp.add(new PieEntry(1240f, 3));
-        NoOfEmp.add(new PieEntry(1369f, 4));
-        NoOfEmp.add(new PieEntry(1487f, 5));
-        NoOfEmp.add(new PieEntry(1501f, 6));
-        NoOfEmp.add(new PieEntry(1645f, 7));
-        NoOfEmp.add(new PieEntry(1578f, 8));
-        NoOfEmp.add(new PieEntry(1695f, 9));
-        PieDataSet dataSet = new PieDataSet(NoOfEmp, "Number Of Employees");
-
-        List<String> year = new ArrayList();
-        year.add("2008");
-        year.add("2009");
-        year.add("2010");
-        year.add("2011");
-        year.add("2012");
-        year.add("2013");
-        year.add("2014");
-        year.add("2015");
-        year.add("2016");
-        year.add("2017");
-
+        PieDataSet dataSet = new PieDataSet(entries, "Money Raised for Charities");
         PieData data = new PieData(dataSet);
         pieChart.setData(data);
+
         dataSet.setColors(ColorTemplate.COLORFUL_COLORS);
         pieChart.animateXY(5000, 5000);
         pieChart.invalidate(); // refresh
